a28b41865e5b70becdc6ddb93dc8a5f4d2111196
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-11-24 16:11:35 +0000

    ticketCard: create component

--- app/components/ticketCard.tsx
+++ app/components/ticketCard.tsx
@@ -8,20 +8,20 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import { Badge } from "~/components/ui/badge";
 
 export default function TicketCard() {
   return (
-    <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-sm relative-z-0 bg-linear-to-r from-purple-200 to-purple-400">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
+        <CardTitle className="text-start text-lg">All Lines (Zone A)</CardTitle>
+        <CardDescription className="text-start text-md">
           Enter your email below to login to your account
         </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent className="flex flex-row-reverse flex-wrap">
+        <Badge className="z-2">30 Days</Badge>
+      </CardContent>
       <CardFooter className="flex-col gap-2"></CardFooter>
     </Card>
   );
