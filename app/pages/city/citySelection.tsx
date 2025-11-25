e6d3e17d554355f6a14709580687f5bf0d93a843
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-11-25 12:10:18 +0000

    city selection: more xamples + styling

--- app/pages/city/citySelection.tsx
+++ app/pages/city/citySelection.tsx
@@ -4,17 +4,38 @@ import { useNavigate } from "react-router";
 export default function CitySelectionPage() {
   const navigate = useNavigate();
   let cities = [
-    "Wroclaw",
-    "Krakow",
-    "Warsaw",
-    "Lodz",
-    "Gdansk",
+    "Warszawa",
+    "Kraków",
+    "Wrocław",
+    "Łódź",
+    "Poznań",
+    "Gdańsk",
     "Szczecin",
+    "Lublin",
     "Bydgoszcz",
+    "Białystok",
+    "Katowice",
+    "Gdynia",
+    "Częstochowa",
+    "Radom",
+    "Rzeszów",
+    "Toruń",
+    "Sosnowiec",
+    "Kielce",
+    "Gliwice",
+    "Olsztyn",
+    "Zabrze",
+    "Bielsko-Biała",
+    "Bytom",
+    "Zielona Góra",
+    "Rybnik",
   ];
+
   return (
-    <div className="h-screen w-screen flex flex-col flex-grow items-center">
-      <h1 className="w-full h-24 p-2 m-2"> please select you city </h1>
+    <div className="h-screen w-screen flex flex-col items-center justify-center overflow-hidden">
+      <h1 className="h-24 p-2 m-2 font-semibold text-2xl">
+        please select you city
+      </h1>
       <CitySelector
         cities={cities}
         onCitySelect={(city) => navigate(`/${city}`)}
