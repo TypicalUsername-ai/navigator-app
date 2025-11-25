54745bc88edd08ae1e9eb3eeaf0dd6af77a4870e
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-11-25 12:10:30 +0000

    mode selection: city callout & styling

--- app/pages/city/modeSelection.tsx
+++ app/pages/city/modeSelection.tsx
@@ -1,14 +1,18 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { Button } from "~/components/ui/button";
 import TicketDrawer from "~/components/ticketDrawer";
 
 export default function ModeSelectionPage() {
+  const params = useParams();
   const buttonSize = "lg";
   const classes = "p-1 m-1 w-full max-w-64";
   const navigate = useNavigate();
   return (
     <div className="h-screen flex flex-grow flex-col justify-around items-center">
-      <div> How are we traveling today? </div>
+      <div className="flex flex-col gap-1">
+        <h1 className="font-bold text-3xl">{params.city},</h1>
+        <h1 className="font-semibold text-xl"> How are we traveling today? </h1>
+      </div>
       <div>
         <Button
           onClick={() => navigate("parking")}
