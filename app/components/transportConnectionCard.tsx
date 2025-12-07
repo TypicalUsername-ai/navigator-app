36cbfc925db70d60c7a659baffa80721004d284e
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-12-07 10:25:39 +0000

    connection card and search page impl

---
+++ app/components/transportConnectionCard.tsx
@@ -0,0 +1,70 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardAction,
+  CardTitle,
+  CardFooter,
+} from "~/components/ui/card";
+import { Badge } from "~/components/ui/badge";
+import {
+  Footprints,
+  BusFront,
+  TramFront,
+  Route,
+  SquareM,
+  SquareArrowDownRight,
+  MoveRight,
+  Repeat,
+} from "lucide-react";
+export default function TransportConnectionCard() {
+  return (
+    <Card className="aspect-5/1 gap-1 p-2">
+      <CardContent className="items-between flex h-full w-full flex-row p-0">
+        <div className="m-2 place-self-end text-lg font-bold lg:text-3xl">
+          18min
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <BusLine nr={238} />
+        </div>
+      </CardContent>
+      <CardFooter className="items-between flex flex-row gap-3">
+        <Walk distance={400} />
+        <Badge>Jordanowska</Badge>
+        <Stops stops={10} />
+        <Badge>Rodno Matecznego</Badge>
+        <Walk distance={200} />
+      </CardFooter>
+    </Card>
+  );
+}
+
+function Walk({ distance }: { number }) {
+  return (
+    <div className="flex flex-col items-center">
+      <Footprints size={16} />
+      <p className="text-light text-xs">{distance}m</p>
+    </div>
+  );
+}
+
+function Stops({ stops }: { number }) {
+  return (
+    <div className="flex flex-col items-center">
+      <Route size={24} />
+      <p className="text-light text-xs">{stops} stops</p>
+    </div>
+  );
+}
+
+function BusLine({ nr }: { number }) {
+  return (
+    <div className="flex flex-row gap-1">
+      <BusFront size={32} />
+      <Badge variant="ou1tline" className="border-primary rounded-sm border-1">
+        137
+      </Badge>
+    </div>
+  );
+}
--- app/pages/transport/transportSearchPage.tsx
+++ app/pages/transport/transportSearchPage.tsx
@@ -3,13 +3,16 @@ import TransportRouteForm, {
 } from "~/components/transportRouteForm";
 import RoutesQuickAccess from "~/components/routesQuickAccess";
 import TicketsQuickAccess from "~/components/ticketsQuickAccess";
+import TransportConnectionCard from "~/components/transportConnectionCard";
 
 export default function TransportSearchPage({
   city,
 }: TransportSearchPageParams) {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-start gap-6 p-3">
-      AAAA
+      <div className="">
+        <TransportConnectionCard />
+      </div>
     </div>
   );
 }
